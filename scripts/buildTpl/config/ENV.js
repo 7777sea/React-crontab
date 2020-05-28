60710667028ab6af3f5aecbb2d7e7474de79bb45
leo <923384777@qq.com>
2020-05-28 18:37:25 +0800

    change info

--- scripts/buildTpl/config/ENV.js
+++ scripts/buildTpl/config/ENV.js
@@ -3,7 +3,6 @@
  * @type {{apiDomain}}
  */
 var ENV = (function () {
-    var docpDomain = "${docpDomain}";
     var apiDomain = "${apiDomain}";
 
     var ssoLoginUrl = "${ssoLoginUrl}";
@@ -11,13 +10,11 @@ var ENV = (function () {
     var rootPath = "${rootPath}";
     return {
         rootPath: rootPath,                                 // 根路由前缀
-        docpDomain: docpDomain,
         ssoLoginUrl: ssoLoginUrl,
         apiDomain: apiDomain,
         login:{
             isStartLoginCheck: false,
             defaultRedirectUrl: rootPath + "reception",     // 登录成功后默认跳转页面
-            loginUrl: docpDomain + `/#/login?service=${ssoLoginUrl}`,   // 登录页面路由
         }
     }
 })();
